ae218145406852c5faf1dfed491a483137ec16ef
bonvy <francesco.bonvecchio01@gmail.com>
2025-01-15 22:36:51 +0100

    first

---
+++ apps/login/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  imports: [NxWelcomeComponent, RouterModule],
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less',
+})
+export class AppComponent {
+  title = 'login';
+}
