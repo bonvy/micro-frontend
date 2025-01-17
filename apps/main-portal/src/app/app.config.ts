c1beca2989a554bfdbee91e2b6f977750c6cb808
bonvy <francesco.bonvecchio01@gmail.com>
2025-01-17 14:35:01 +0100

    save

--- apps/main-portal/src/app/app.config.ts
+++ apps/main-portal/src/app/app.config.ts
@@ -1,15 +1,15 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    importProvidersFrom(ServiceWorkerModule.register('ngsw-worker.js', {
+    provideServiceWorker('ngsw-worker.js', {
       enabled: true,
-      registrationStrategy: 'registerWhenStable:3000'
-    })),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     provideRouter(appRoutes),
   ],
 };
